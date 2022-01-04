190146ee7ccb36dd1bec3f5a9c476c878770812a
MichaelBodendorf <bodi@gmx.at>
2022-01-04 17:24:49 +0100

    completed 05

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -5,7 +5,18 @@ describe Oystercard do
   it "is able to store the customers balance" do
     
     expect(subject.balance).to eq 0
-    
+
   end
+
+  it "responds to top_up method" do
+    expect(subject).to respond_to(:top_up).with(1).argument
+
+  end
+
+  it "increases the balance by the top up value" do
+    expect{ subject.top_up 5 }.to change{ subject.balance }.by 5
+    # expect(subject.top_up(5)).to eq 5
+  end
+
 end
 
